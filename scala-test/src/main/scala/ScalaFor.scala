9a9fc2038cc5e19837ca1ee9e751fcfae858fa33
김경민 <kkm07133@naver.com>
2025-04-09 23:34:16 +0900

    스칼라 반복문 테스트

---
+++ scala-test/src/main/scala/ScalaFor.scala
@@ -0,0 +1,35 @@
+import java.text.DateFormat.{LONG, getDateInstance}
+import java.util.{Date, Locale}
+
+object ScalaFor {
+  def main(args: Array[String]): Unit = {
+    val now = new Date
+    val df = getDateInstance(LONG, Locale.FRANCE)
+    val list: List[Int] = List(13,4,5,5,4,4,7,8,3)
+    println(df format now)
+
+    // ...?
+    val value:Int = 1.*(10)
+    println(value)
+
+    // for (to)
+    // 1 ~ 5
+    for(i <- 1 to 5) {
+      println(i)
+    }
+
+    // for (until)
+    // 1 ~ 4
+    for(i <- 1 until 5) {
+      println(i)
+    }
+
+    // 중첩 for
+    for {
+      i <- 1 to 3
+      j <- 1 to 2
+    } println("hello" + i + j)
+
+    for (value <- list) println(value)
+  }
+}
