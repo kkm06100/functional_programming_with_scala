864a7c97f2f2b38834e35666eef373f2a7144c63
김경민 <kkm07133@naver.com>
2025-04-11 19:32:01 +0900

    Lazy 리스트 사용

---
+++ scala-test/src/main/scala/artofscala/LazyPrimeExample.scala
@@ -0,0 +1,15 @@
+package artofscala
+
+object LazyPrimeExample {
+  val naturals: LazyList[Int] = LazyList.from(2)
+
+  def sieve(s: LazyList[Int]): LazyList[Int] =
+    s.head #:: sieve(s.tail.filter(_ % s.head != 0))
+
+  val primes: LazyList[Int] = sieve(naturals)
+
+  def main(args: Array[String]): Unit = {
+    println(naturals.take(30).toList)
+    println(primes.take(30).toList)
+  }
+}
