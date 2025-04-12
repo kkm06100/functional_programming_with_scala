a5d27aa46773f39192994818faf66d26b3760f93
김경민 <kkm07133@naver.com>
2025-04-12 21:17:19 +0900

    StateMachineDSL 추가

---
+++ scala-test/src/main/scala/dsl/statemachine/StateMachineDSL.scala
@@ -0,0 +1,40 @@
+package dsl.statemachine
+
+import dsl.statemachine.status.{State, Event}
+
+class StateMachineDSL(transitions: Map[(State, Event), State]) {
+  def transition(current: State, event: Event): State =
+    transitions.getOrElse((current, event), current)
+}
+
+object StateMachineDSL {
+  def dsl(build: Builder => Unit): StateMachineDSL = {
+    val builder = new Builder
+    build(builder)
+    new StateMachineDSL(builder.build())
+  }
+
+  class Builder {
+    private var transitions = Map.empty[(State, Event), State]
+
+    def state(s: State)(build: StateBuilder => Unit): Unit = {
+      val sb = new StateBuilder(s)
+      build(sb)
+      transitions ++= sb.transitions
+    }
+
+    def build(): Map[(State, Event), State] = transitions
+  }
+
+  class StateBuilder(state: State) {
+    var transitions = Map.empty[(State, Event), State]
+
+    def on(event: Event): EventBuilder = new EventBuilder(state, event, this)
+
+    class EventBuilder(s: State, e: Event, parent: StateBuilder) {
+      def goTo(target: State): Unit = {
+        parent.transitions += ((s, e) -> target)
+      }
+    }
+  }
+}
