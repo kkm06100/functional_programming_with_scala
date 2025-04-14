42edbeaec988d472a19085ec44609dfce12171c3
김경민 <kkm07133@naver.com>
2025-04-15 08:31:50 +0900

    substraction 추가

--- scala-test/src/main/scala/dsl/directstyle/expression/ExpressionDSL.scala
+++ scala-test/src/main/scala/dsl/directstyle/expression/ExpressionDSL.scala
@@ -1,17 +1,19 @@
 package dsl.directstyle.expression
 
-import dsl.directstyle.expression.status.{Add, Divide, Expression, Multiply, Number}
+import dsl.directstyle.expression.status.{Add, Divide, Subtraction, Expression, Multiply, Number}
 
 object ExpressionDSL {
 
   def $(n: Int): Expression = Number(n)
   def +(a: Expression, b: Expression): Expression = Add(a, b)
+  def -(a: Expression, b: Expression): Expression = Subtraction(a, b)
   def *(a: Expression, b: Expression): Expression = Multiply(a, b)
   def /(a: Expression, b: Expression): Expression = Divide(a, b)
 
   def eval(expression: Expression): Int = expression match{
     case Number(v) => v
     case Add(l, r)   => eval(l) + eval(r)
+    case Subtraction(l, r) => eval(l) - eval(r)
     case Multiply(l, r) => eval(l) * eval(r)
     case Divide(l, r) => eval(l) / eval(r)
   }
--- scala-test/src/main/scala/dsl/directstyle/expression/ExpressionDSLUsage.scala
+++ scala-test/src/main/scala/dsl/directstyle/expression/ExpressionDSLUsage.scala
@@ -5,7 +5,7 @@ import ExpressionDSL._
 object ExpressionDSLUsage {
 
   def main(array: Array[String]): Unit = {
-    val expression = /(*($(10), $(20)), /($(10), $(5))) // similar to lisp...
+    val expression = /(*($(10), $(20)), `-`($(10), $(5))) // similar to lisp...
     println(eval(expression))
   }
 }
