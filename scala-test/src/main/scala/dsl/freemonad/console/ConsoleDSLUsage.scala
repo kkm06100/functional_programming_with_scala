03f4082dcde99b14ab028e0435fdea8724fc0b68
김경민 <kkm07133@naver.com>
2025-04-15 08:33:48 +0900

    ConsoleDSL 인터프리터 및 테스트

---
+++ scala-test/src/main/scala/dsl/freemonad/console/ConsoleDSLUsage.scala
@@ -0,0 +1,25 @@
+package dsl.freemonad.console
+
+import cats.effect.{IO, IOApp}
+import cats.~>
+import dsl.freemonad.console.ConsoleDSL.{ConsoleProgram, printLine, readLine}
+import dsl.freemonad.console.adt.{ConsoleOp, PrintLine, ReadLine}
+
+object ConsoleDSLUsage extends IOApp.Simple {
+  val program: ConsoleProgram[Unit] = for {
+    name <- readLine("이름을 입력하세요:")
+    _    <- printLine(s"안녕하세요, $name 님!")
+  } yield ()
+
+  val interpreter = new (ConsoleOp ~> IO) {
+    def apply[A](fa: ConsoleOp[A]): IO[A] = fa match {
+      case ReadLine(prompt) =>
+        IO.print(prompt) *> IO.readLine
+      case PrintLine(msg) =>
+        IO.println(msg)
+    }
+  }
+
+  def run: IO[Unit] =
+    program.foldMap(interpreter)
+}
